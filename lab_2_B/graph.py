9850acafcab520bd30a6e26e9f5b3123e5943266
AmmarSaqib <ammarsaqib123@gmail.com>
2021-04-08 11:18:54 +0500

    lab 2 section B

--- lab_2_B/graph.py
+++ lab_2_B/graph.py
@@ -7,7 +7,6 @@ class Graph:
 
     def __init__(self):
         self.edges = {} # dictionary of edges NODE: NEIGHBOURS
-        self.weights = {} # dictionary of NODES and their COSTS
 
     def neighbours(self, node):
         """
@@ -20,18 +19,6 @@ class Graph:
         """
 
         return self.edges[node]
-
-    def get_cost(self, from_node, to_node):
-        """
-        Gets the cost of a connection between adjacent nodes.
-
-        :params from_node: (string) starting node
-        :params to_node: (string) ending node
-
-        :return: (int) 
-        """
-        
-        return self.weights[(from_node + to_node)]
 
 
 
@@ -50,16 +37,4 @@ if __name__ == "__main__":
         'G': set(['F','E','C'])
     }
 
-    # setting up connection costs
-    graph.weights = {
-        'AB': 5, 'AD': 3,
-        'BA': 5, 'BE': 4, 'BC': 1,
-        'CB': 1, 'CE': 6, 'CG': 8,
-        'DA': 3, 'DE': 2, 'DF': 2,
-        'EB': 4, 'EC': 6, 'ED': 2, 'EG': 4,
-        'FD': 2, 'FG': 3,
-        'GF': 3, 'GE': 4, 'GC': 8
-    }
-
-    print("Neighbours of Node A are:",graph.neighbours('A'))
     print("Cost going from A to D is:", graph.get_cost('A','D'))
---
+++ lab_2_B/lab_2.py
@@ -0,0 +1,59 @@
+"""
+Lab Task: 
+
+For this lab you will be implementing Depth First on a graph.
+
+For this lab you are provided with one helper classes.
+It is for making a graph.
+You may open then the file, once opened you'll see descriptions for each function defined
+and a testing code for the class in the main body. Kindly go through them to get comfortable 
+with the functions
+
+MAIN TASK:
+The graph provided has already been created below and the DFS (Depth First Search) function has been called.
+You are suppose to fill in the DFS function declared below. A description of what the function should do is mentioned.
+Use functions from the helper class in order to write the UCS function.
+Your function should print the path. 
+"""
+
+# importing helper classes
+from graph import Graph
+
+
+def depth_first_search(graph, start_node):
+    """
+    The function should take in the graph defined along with the
+    start node and print out the path according 
+    to the Depth First Search Algorithm.
+
+    NOTE: print the path
+
+    :params graph: (Graph) defined graph
+    :params start_node: (String) starting node from graph
+
+    :return : None
+    """
+
+    # write your code below
+
+    pass # remove this line when coding
+
+if __name__ == "__main__":
+
+    # Defining Graph
+    graph = Graph()
+
+    # setting up nodes and neighbours
+    graph.edges = {
+        'A': set(['B', 'D']),
+        'B': set(['A','E','C']),
+        'C': set(['B', 'E', 'G']),
+        'D': set(['A','E','F']),
+        'E': set(['B', 'C', 'D', 'G']),
+        'F': set(['D','G']),
+        'G': set(['F','E','C'])
+    }
+
+    # setting up connection costs
+
+    depth_first_search(graph, 'A') 
