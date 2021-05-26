2797b9ff2997bf5e753bea532acf6d991d7a0518
AmmarSaqib <ammarsaqib123@gmail.com>
2021-05-26 13:56:33 +0500

    lab 5 added

---
+++ lab_5/lab_5.py
@@ -0,0 +1,20 @@
+"""
+Lab: 5
+
+
+For this lab you'll be working with a simple implementation of a Genetic Algorithm.
+In order to make things easy we'll use a starter code of GA implementation that can
+be found on this link: https://www.geeksforgeeks.org/genetic-algorithms/
+
+For this task you'll understand the code in the link and modify to the following 
+problem.
+
+Consider 16 locations: A, B, C, D, E, F, G, H, I, J, K, L, M, O, P
+each of these locations can be accessed from one another. The optimal path to take
+is: A, C, J, L, M, P. 
+Modify the code in the link to implement GA on this problem.
+Define what is going to be the length of the chromosome, the genes, do you need to change the
+fitness function or not. Does increasing the population size or decreasing it effects the 
+number of generations it takes to reach the goal.
+"""
+
